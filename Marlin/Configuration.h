f48f9bb84de143fdf51b8539ac530daf38d09a3d
SteveParry <stevedparry@outlook.com>
2021-09-23 17:41:51 +0100

    changed z offset to match new printed mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1156,7 +1156,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -6, -4.1 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -6, -4.41 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
